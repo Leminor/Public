8051a3eb935f1971b089ec331e357ec97bcf1b01
Your Name <you@example.com>
2021-02-08 22:10:37 +0300

    L5 added user int

---
+++ L5/user_interract.php
@@ -0,0 +1,8 @@
+<?php
+$name = readline ("Enter your name: ");
+$pass = readline ("Enter your pass: ");
+$age = readline ("Enter your age: ");
+var_dump($name, $pass, $age);
+
+echo "Hello {$name}, {$age}. Dont forgot your pass: {$pass}", PHP_EOL;
+
