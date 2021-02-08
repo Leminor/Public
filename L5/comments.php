d7e97d960290d22aca3d6f6437bf596a59684ae5
Your Name <you@example.com>
2021-02-08 21:04:09 +0300

    L5 added comments

---
+++ L5/comments.php
@@ -0,0 +1,20 @@
+<?php
+
+//echo 'test1', PHP_EOL;
+#echo 'test2', PHP_EOL;
+echo 'test3', PHP_EOL;
+/*
+echo 'test4', PHP_EOL;
+echo 'test5', PHP_EOL;
+*/
+echo 'test6', PHP_EOL;
+
+/**
+ * @param $testA
+ * @param $testB
+ * @return string
+ */
+function testSMTH($testA, $testB)
+{
+    return "test"
+}
