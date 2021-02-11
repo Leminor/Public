9898e7967f0c7234a45256180c01d5a34f14631c
Your Name <you@example.com>
2021-02-11 21:08:40 +0300

    L6 added constants

---
+++ L6/constants.php
@@ -0,0 +1,18 @@
+<?php
+
+define('APP_VERSION', '3.0.1');
+echo APP_VERSION, PHP_EOL;
+
+const POWER = '220v';
+Echo POWER, PHP_EOL;
+
+const MY_COEFFICIENT = 3.1;
+$salary = 2000;
+echo $salary * MY_COEFFICIENT, PHP_EOL;
+
+
+#Magic constants
+
+echo __DIR__; PHP_EOL;
+echo __FILE__; PHP_EOL;
+echo __LINE__; PHP_EOL;
