161c9d8c98a7da64562758505c6d8bfc94bd13ff
Your Name <you@example.com>
2021-02-11 20:42:09 +0300

    L6 added strings1

---
+++ L6/strings.php
@@ -0,0 +1,30 @@
+<?php
+echo ('TEST');
+echo 'Hello world';
+echo 1;
+
+print 'Smth';
+print PHP_EOL;
+
+
+$name = 'Alex';
+echo "Hello {$name}\n";
+echo 'Hello $name\n' PHP_EOL;
+
+$htmlid = 1;
+
+$nowdoc = <<<'JS'
+document.getElementById({$htmlid})
+JS;
+
+$heredoc = <<<HTML
+<script type="text/javascript">
+document.getElementById('#{$htmlid}').innerHTML;
+</script>
+HTML;
+echo $heredoc, PHP_EOL;
+
+echo "Hello user with name \"{$name}\"\n";
+
+
+
