5ff17114b6159b5d3d6ef351082cd09365273079
Your Name <you@example.com>
2021-02-11 21:26:39 +0300

    L6 added math

---
+++ L6/math.php
@@ -0,0 +1,16 @@
+<?php
+
+echo 1 + 2, PHP_EOL;
+echo 1 - 2, PHP_EOL;
+echo 1 / 2, PHP_EOL;
+echo 543 * 0.2, PHP_EOL;
+
+echo 3 % 5, PHP_EOL;
+echo 3 / 5, PHP_EOL;
+echo 3 ** 2, PHP_EOL;
+
+$result = 0;
+$result = $result + 2;
+$result += 2;
+echo $result, PHP_EOL;
+
